a89c0fb14795656f3174e4ed0a88c856ff0ec6b0
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-09-27 14:22:34 -0300

    React Hooks #09 useReducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import { useReducer } from "react";
 
-const UseReducer = (props) => {
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
-        </div>
-    )
+const initialState = {
+  other: "...",
+  user: null,
+  number: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "numberAdd2":
+      return { ...state, number: state.number + 2 };
+    case "login":
+      return { ...state, user: { name: action.payload } };
+    default:
+      return state;
+  }
 }
 
-export default UseReducer
+const UseReducer = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div className="UseReducer">
+      <PageTitle
+        title="Hook UseReducer"
+        subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+
+      <div className="center">
+        {state.user ? (
+          <span className="text">{state.user.name}</span>
+        ) : (
+          <span className="text">Sem usuário</span>
+        )}
+        <span className="text">{state.number}</span>
+        <div>
+          <button className="btn" onClick={() => dispatch({ type: "numberAdd2" })}>
+            +2
+          </button>
+          <button className="btn" onClick={() => dispatch({ type: "login", payload: 'Bianca' })}>
+            User
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseReducer;
